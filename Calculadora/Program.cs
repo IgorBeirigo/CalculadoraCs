86b7ac762db3bd31fc26571f822e579cf78dde93
Igor Fernandes <igorfernandes.b.silva12345@hotmail.com>
2025-07-01 19:15:46 -0300

    Primeiro commit: adicionando código e solução

---
+++ Calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Calculadora
+{
+    class Program
+    {
+        enum Menu { soma = 1, subtracao , multiplicacao, divisao, potencia, raiz, sair };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seja bem vindo ao CALC, Selecione uma das opções:");
+            Console.WriteLine("1-soma\n2-subtração\n3-multiplicação\n4-divisão\n5-potência\n6-raiz\n7-sair");
+
+            Menu opcao = (Menu)int.Parse(Console.ReadLine());
+
+            Console.WriteLine(opcao);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
