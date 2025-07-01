4fc999de9529dc7251bab393182390d2c8e2247e
Igor Fernandes <igorfernandes.b.silva12345@hotmail.com>
2025-07-01 19:42:45 -0300

    :sparkles: feat: nova função SAIR na calculadora

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -12,16 +12,31 @@ namespace Calculadora
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Seja bem vindo ao CALC, Selecione uma das opções:");
-            Console.WriteLine("1-soma\n2-subtração\n3-multiplicação\n4-divisão\n5-potência\n6-raiz\n7-sair");
+            bool escolheuSair = false;
+            while (!escolheuSair) // enquanto o usuario não (!) escolher sair exiba o menu
+            {
+                Console.WriteLine("Seja bem vindo ao CALC, Selecione uma das opções:");
+                Console.WriteLine("1-soma\n2-subtração\n3-multiplicação\n4-divisão\n5-potência\n6-raiz\n7-sair");
 
-            Menu opcao = (Menu)int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)int.Parse(Console.ReadLine());
 
-            Console.WriteLine(opcao);
+                switch (opcao)
+                {
+                    case Menu.sair:
+                        escolheuSair = true;
+                        Console.WriteLine("Obrigado por usar o CALC!");
+                        break;
+                }
+
+                Console.WriteLine(opcao);
+
+                Console.Clear();
+
+                
+            }
 
 
-
-            Console.ReadLine();
+        
         }
     }
 }
